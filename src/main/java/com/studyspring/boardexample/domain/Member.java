fc9fcca9121084f63a66c16ee2b02aeb6d8672f3
d0lim <limdongyoung0@gmail.com>
2022-07-10 17:20:16 +0900

    Tidy annotation for Member entity

--- src/main/java/com/studyspring/boardexample/domain/Member.java
+++ src/main/java/com/studyspring/boardexample/domain/Member.java
@@ -1,17 +1,18 @@
 package com.studyspring.boardexample.domain;
 
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "members")
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
     @Id
     @GeneratedValue
