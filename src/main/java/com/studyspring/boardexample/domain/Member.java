69b74adc588dc1e1a6d48c09eacaf0f74f154add
d0lim <limdongyoung0@gmail.com>
2022-07-10 18:59:24 +0900

    Apply getter to Member entity

--- src/main/java/com/studyspring/boardexample/domain/Member.java
+++ src/main/java/com/studyspring/boardexample/domain/Member.java
@@ -2,6 +2,7 @@ package com.studyspring.boardexample.domain;
 
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "members")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class Member {
 
     @Id
