1f6fc5cf9ac2fb2e2d79475c703b3b6d265fb664
d0lim <limdongyoung0@gmail.com>
2022-07-10 18:03:54 +0900

    Apply EnumValue to EnumController

--- src/main/java/com/studyspring/boardexample/controller/EnumController.java
+++ src/main/java/com/studyspring/boardexample/controller/EnumController.java
@@ -1,11 +1,16 @@
 package com.studyspring.boardexample.controller;
 
+import com.studyspring.boardexample.constants.EnumModel;
+import com.studyspring.boardexample.constants.EnumValue;
 import com.studyspring.boardexample.domain.UserRole;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class EnumController {
@@ -17,5 +22,21 @@ public class EnumController {
         enums.put("userRole", userRole.getEnumConstants());
 
         return enums;
+    }
+
+    @GetMapping("/value")
+    public Map<String, List<EnumValue>> getEnumValue() {
+        Map<String, List<EnumValue>> enumValues = new LinkedHashMap<>();
+
+        enumValues.put("userRole", toEnumValues(UserRole.class));
+
+        return enumValues;
+    }
+
+    private List<EnumValue> toEnumValues(Class<? extends EnumModel> e) {
+        return Arrays
+                .stream(e.getEnumConstants())
+                .map(EnumValue::new)
+                .collect(Collectors.toList());
     }
 }
